ea3c8ae79ebd9c3fa68c7054eb4a514a622bc641
renualdmarch <renuald@email.com>
2014-07-17 22:48:12 +0300

    created mission engine.

--- src/main/scala/mr/merc/world/mission/Mission.scala
+++ src/main/scala/mr/merc/world/mission/Mission.scala
@@ -12,27 +12,24 @@ import java.time.Period
 import mr.merc.world.WorldState
 
 sealed trait Mission {
-  def startDate: LocalDate
-  def endDate: LocalDate
-  def daysForExecution = Period.between(startDate, endDate).getDays()
   def difficulty: Difficulty
   def location: Province
   def comparingObj: AnyRef
 }
 
-case class DefendFromEnemyMission(startDate: LocalDate, endDate: LocalDate, difficulty: Difficulty, location: Province, enemyProvince: Province) extends Mission {
+case class DefendFromEnemyMission(difficulty: Difficulty, location: Province, enemyProvince: Province) extends Mission {
   def comparingObj = (location, enemyProvince)
 }
-case class AttackEnemyMission(startDate: LocalDate, endDate: LocalDate, difficulty: Difficulty, location: Province, enemyProvince: Province) extends Mission {
+case class AttackEnemyMission(difficulty: Difficulty, location: Province, enemyProvince: Province) extends Mission {
   def comparingObj = (location, enemyProvince)
 }
-case class SupressRiotMission(startDate: LocalDate, endDate: LocalDate, difficulty: Difficulty, location: Province) extends Mission {
+case class SupressRiotMission(difficulty: Difficulty, location: Province) extends Mission {
   def comparingObj = location
 }
-case class KillAnimalsMission(startDate: LocalDate, endDate: LocalDate, difficulty: Difficulty, location: Province) extends Mission {
+case class KillAnimalsMission(difficulty: Difficulty, location: Province) extends Mission {
   def comparingObj = location
 }
-case class GuardCaravanMission(startDate: LocalDate, endDate: LocalDate, difficulty: Difficulty, location: Province) extends Mission {
+case class GuardCaravanMission(difficulty: Difficulty, location: Province) extends Mission {
   def comparingObj = location
 }
 
@@ -58,8 +55,6 @@ object MissionGenerators {
 
   def attackEnemyMission(ctx: MissionContext): Option[AttackEnemyMission] = {
     val diff = bounds(classOf[AttackEnemyMission])
-    val startDate = ctx.worldState.calendar.today
-    val endDate = endDateByStartDate(startDate)
     val currentCountry = ctx.worldState.map.countryByProvince(ctx.location)
     val neigs = ctx.worldState.map.neighbours(ctx.location).map(ctx.worldState.map.countryByProvince)
     val enemies = neigs.filter(n => ctx.worldState.diplomacyEngine.areEnemies(currentCountry, n))
@@ -68,14 +63,12 @@ object MissionGenerators {
     } else {
       val enemy = enemies.randomElement()
       val enemyProvince = ctx.worldState.map.neighboursByCountry(ctx.location, enemy).randomElement()
-      Some(AttackEnemyMission(startDate, endDate, diff.generateDifficulty, ctx.location, enemyProvince))
+      Some(AttackEnemyMission(diff.generateDifficulty, ctx.location, enemyProvince))
     }
   }
 
   def defendFromEnemyMission(ctx: MissionContext): Option[DefendFromEnemyMission] = {
     val diff = bounds(classOf[DefendFromEnemyMission])
-    val startDate = ctx.worldState.calendar.today
-    val endDate = endDateByStartDate(startDate)
     val currentCountry = ctx.worldState.map.countryByProvince(ctx.location)
     val neigs = ctx.worldState.map.neighbours(ctx.location).map(ctx.worldState.map.countryByProvince)
     val enemies = neigs.filter(n => ctx.worldState.diplomacyEngine.areEnemies(currentCountry, n))
@@ -84,22 +77,18 @@ object MissionGenerators {
     } else {
       val enemy = enemies.randomElement()
       val enemyProvince = ctx.worldState.map.neighboursByCountry(ctx.location, enemy).randomElement()
-      Some(DefendFromEnemyMission(startDate, endDate, diff.generateDifficulty, ctx.location, enemyProvince))
+      Some(DefendFromEnemyMission(diff.generateDifficulty, ctx.location, enemyProvince))
     }
   }
 
   def supressRiotMission(ctx: MissionContext): Option[SupressRiotMission] = {
     val diff = bounds(classOf[SupressRiotMission])
-    val startDate = ctx.worldState.calendar.today
-    val endDate = endDateByStartDate(startDate)
-    Some(SupressRiotMission(startDate, endDate, diff.generateDifficulty, ctx.location))
+    Some(SupressRiotMission(diff.generateDifficulty, ctx.location))
   }
 
   def killAnimals(ctx: MissionContext): Option[KillAnimalsMission] = {
     val diff = bounds(classOf[KillAnimalsMission])
-    val startDate = ctx.worldState.calendar.today
-    val endDate = endDateByStartDate(startDate)
-    Some(KillAnimalsMission(startDate, endDate, diff.generateDifficulty, ctx.location))
+    Some(KillAnimalsMission(diff.generateDifficulty, ctx.location))
   }
 }
 
